08641b001d3c4a2183bccab9240dc7141e112484
bracebrace008 <142418145+bracebrace008@users.noreply.github.com>
2024-12-16 23:16:43 +0800

    feat: tsconfig配置

--- fetchFigma.js
+++ fetchFigma.js
@@ -1,56 +1,27 @@
 const axios = require("axios");
 
+const FIGMA_API_URL = "https://api.figma.com/v1/files/";
+const FILE_KEY = "MbUzcnUAQ8QER5qFcWlcjQ";
+const ACCESS_TOKEN = process.env.FIGMA_ACCESS_TOKEN;
+console.log(ACCESS_TOKEN);
 
 async function fetchFigmaFile() {
+  const node_id = "12:9";
   try {
     const response = await axios.get(
-      `${FIGMA_API_URL}${FILE_KEY}/nodes?ids=54:69&depth=2`,
+      `${FIGMA_API_URL}${FILE_KEY}/nodes?ids=${node_id}&depth=2`,
       {
         headers: {
           "X-Figma-Token": ACCESS_TOKEN,
         },
       }
     );
-    delete response.data["nodes"]["54:69"].document.children;
-    console.log(JSON.stringify(response.data["nodes"]["54:69"].document));
+    delete response.data["nodes"][node_id].document.children;
+    console.log(JSON.stringify(response.data["nodes"][node_id].document));
     // console.log(JSON.stringify(response.data["nodes"]["54:69"]));
   } catch (error) {
-    console.error("Error fetching Figma file:", error);
+    // console.error("Error fetching Figma file:", error);
   }
 }
 
 fetchFigmaFile();
-
-function parseFigmaNodeToReactComponent(node) {
-  if (!node || !node.document) {
-    console.error("Invalid Figma node");
-    return;
-  }
-
-  const { name, type, children } = node.document;
-
-  let component = "";
-
-  switch (type) {
-    case "FRAME":
-    case "GROUP":
-      component += `<div className="container mx-auto">\n`;
-      if (children && children.length > 0) {
-        children.forEach((child) => {
-          component += parseFigmaNodeToReactComponent({ document: child });
-        });
-      }
-      component += `</div>\n`;
-      break;
-    case "TEXT":
-      component += `<p className="text-base">${name}</p>\n`;
-      break;
-    case "RECTANGLE":
-      component += `<div className="bg-gray-200 w-full h-64"></div>\n`;
-      break;
-    default:
-      console.warn(`Unsupported node type: ${type}`);
-  }
-
-  return component;
-}
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { colorToRgba } from "./utils/colorUtil";
+
+console.log(
+  colorToRgba({
+    r: 0.9333333373069763,
+    g: 0.9333333373069763,
+    b: 0.9333333373069763,
+    a: 1,
+  })
+);
