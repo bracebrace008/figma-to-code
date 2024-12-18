06f032ae503b30cf09a8e3904aedce0fc6ba4594
bracebrace008 <142418145+bracebrace008@users.noreply.github.com>
2024-12-18 22:54:02 +0800

    feat: tailwindAutolayout解析

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { paintToCSS } from "./utils/colorUtil";
 import testJson from "./test.json";
+import { getTailwindAutoLayout } from "./tailwind/tailwindAutoLayout";
 
 console.log((testJson as any).fills[0].type);
 
-console.log(paintToCSS((testJson as any).fills[0]));
+console.log(getTailwindAutoLayout((testJson as any)));
---
+++ src/tailwind/tailwindAutoLayout.ts
@@ -0,0 +1,58 @@
+function getFlexDirection(node: FrameNode) {
+  return node.layoutMode === "HORIZONTAL" ? "" : "flex-col";
+}
+
+function getJustifyContent(node: FrameNode) {
+  switch (node.primaryAxisAlignItems) {
+    case "MIN":
+      return "justify-start";
+    case "CENTER":
+      return "justify-center";
+    case "MAX":
+      return "justify-end";
+    case "SPACE_BETWEEN":
+      return "justify-between";
+    default:
+      return "";
+  }
+}
+
+function getAlignItems(node: FrameNode) {
+  switch (node.counterAxisAlignItems) {
+    case "MIN":
+      return "items-start";
+    case "CENTER":
+      return "items-center";
+    case "MAX":
+      return "items-end";
+    case "BASELINE":
+      return "items-baseline";
+    default:
+      return "";
+  }
+}
+
+function getGap(node: FrameNode) {
+  if (
+    node.itemSpacing &&
+    node.itemSpacing > 0 &&
+    node.primaryAxisAlignItems !== "SPACE_BETWEEN"
+  ) {
+    return `gap-[${node.itemSpacing}px]`;
+  }
+}
+
+export function getTailwindAutoLayout(node: FrameNode) {
+  if (node.layoutMode === "NONE") {
+    return "flex-none";
+  }
+  return Object.values({
+    flex: "flex",
+    flexDirection: getFlexDirection(node),
+    justifyContent: getJustifyContent(node),
+    alignItems: getAlignItems(node),
+    gap: getGap(node),
+  })
+    .filter((value) => value != "")
+    .join(" ");
+}
