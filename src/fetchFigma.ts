589583d1c20993654eec0264f25ec2387ff1c4eb
bracebrace008 <142418145+bracebrace008@users.noreply.github.com>
2025-01-09 07:17:33 +0000

    feat: 添加fetchFigma功能并保存Figma和Tailwind内容

---
+++ src/fetchFigma.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+import fs from "fs";
+import { tailwindMain } from "./tailwind/tailwindMain";
+
+const FIGMA_API_URL = "https://api.figma.com/v1/files/";
+const FILE_KEY = "MbUzcnUAQ8QER5qFcWlcjQ";
+const ACCESS_TOKEN = process.env.FIGMA_ACCESS_TOKEN;
+console.log(ACCESS_TOKEN);
+
+export async function fetchFigmaFile() {
+  const node_id = "114:85";
+  try {
+    const response = await axios.get(
+      `${FIGMA_API_URL}${FILE_KEY}/nodes?ids=${node_id}`,
+      {
+        headers: {
+          "X-Figma-Token": ACCESS_TOKEN,
+        },
+      }
+    );
+    const figmaContent = response.data["nodes"][node_id].document;
+    fs.writeFileSync("figma_content.json", JSON.stringify(figmaContent, null, 2));
+    
+    const tailwindContent = await tailwindMain(figmaContent);
+    fs.writeFileSync("tailwind.json", JSON.stringify(tailwindContent, null, 2));
+    
+    console.log("Figma content and Tailwind content have been saved.");
+  } catch (error) {
+    console.error("Error fetching Figma file:", error);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,3 @@
-import { paintToCSS } from "./utils/colorUtil";
-import testJson from "./test.json";
-import { getTailwindAutoLayout } from "./tailwind/tailwindAutoLayout";
-import { getTailwindPadding } from "./tailwind/tailwindPadding";
-import { tailwindMain } from "./tailwind/tailwindMain";
+import { fetchFigmaFile } from "./fetchFigma";
 
-async function main() {
-  const content = await tailwindMain(testJson as any);
-  console.log(content);
-}
-
-main();
+fetchFigmaFile();
